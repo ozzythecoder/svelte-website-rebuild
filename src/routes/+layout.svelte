523bd45a4cbf3aaf251a17b288747b5e16f91158
August McAllister <august.chm@gmail.com>
2023-01-21 19:09:29 -0600

    feat: set title splash to only show on home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
-  import '$lib/styles/style.scss'
+  import '$lib/styles/style.scss';
+  import TitleSplash from '../lib/components/TitleSplash.svelte';
+
+  export let data;
 </script>
+
+{#if data.currentRoute == "/"}
+  <TitleSplash />
+{/if}
 
 <main>
   <slot />
