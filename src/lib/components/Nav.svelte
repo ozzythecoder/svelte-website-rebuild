598b03a1587b2b97c6c075e440e9d369bd58b5f7
August McAllister <august.chm@gmail.com>
2023-01-21 19:03:00 -0600

    feat: animated underline for nav links

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,17 @@
 <nav>
   <ul>
-    <li>ABOUT</li>
-    <li>GALLERY</li>
-    <li>CONTACT</li>
-    <li>HOME</li>
+    <li>
+      <a href="/about"> ABOUT </a>
+    </li>
+    <li>
+      <a href="/gallery"> GALLERY </a>
+    </li>
+    <li>
+      <a href="/contact"> CONTACT </a>
+    </li>
+    <li>
+      <a href="/"> HOME </a>
+    </li>
   </ul>
 </nav>
 
@@ -21,5 +29,31 @@
     font-family: Overpass, system-ui, sans-serif;
     font-weight: 200;
     font-size: 2rem;
+  }
+
+  a {
+    display: inline-block;
+    position: relative;
+    text-transform: uppercase;
+    padding: 5px;
+  }
+
+  a:after {
+    background: none repeat scroll 0 0 transparent;
+    bottom: 0;
+    content: "";
+    display: block;
+    height: 2px;
+    left: 50%;
+    position: absolute;
+    background: #000;
+    transition: width 0.5s ease 0s,
+      left 0.5s ease 0s;
+    width: 0;
+  }
+
+  a:hover:after {
+    width: 100%;
+    left: 0;
   }
 </style>
