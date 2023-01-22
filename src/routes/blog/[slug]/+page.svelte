584e79f43a3159b1ef6bd8b81d3980f2ed669ba4
August McAllister <august.chm@gmail.com>
2023-01-22 13:47:35 -0600

    feat: added dynamic blog routes with date formatting & markdown support

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let data;
+  const { title, date, Content } = data;
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <p>Published on: {date}</p>
+  <Content />
+</article>
+
+<style lang="scss">
+  
+</style>
