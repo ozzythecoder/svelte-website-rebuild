522f7337bae54b86068afe98ecd8fff98f43d6df
August McAllister <august.chm@gmail.com>
2023-01-22 22:21:37 -0600

    feat: created socials bar component; light styling

---
+++ src/lib/components/Socials.svelte
@@ -0,0 +1,44 @@
+<ul>
+  <li>
+    <a href="https://ozzythepainter.bandcamp.com/">
+      <img src="social-icons/bandcamp.png" alt="Bandcamp icon" />
+    </a>
+  </li>
+  <li>
+    <a href="https://open.spotify.com/artist/5Z6R28uhpZFSbuGUYxfFBl">
+      <img src="social-icons/spotify.png" alt="Spotify icon" />
+    </a>
+  </li>
+  <li>
+    <a href="https://instagram.com/ozzythepainter?igshid=Zjc2ZTc4Nzk=">
+      <img src="social-icons/instagram.png" alt="Instagram icon" />
+    </a>
+  </li>
+  <li>
+    <a href="https://twitter.com/ozzythepainter">
+      <img src="social-icons/twitter.png" alt="Twitter icon" />
+    </a>
+  </li>
+</ul>
+
+<style>
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0;
+    margin: 0 30%;
+  }
+
+  li {
+    list-style: none;
+    padding: 0.5rem 1rem 0;
+  }
+
+  img {
+    max-height: 30px;
+
+  }
+
+</style>
