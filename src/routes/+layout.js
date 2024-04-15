c79b348335e266797af9c04a7c434e5d96efacb5
August McAllister <august.chm@gmail.com>
2024-04-15 11:31:41 -0500

    Setup for static generation

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,8 @@
+export const prerender = true;
 export const load = ({ url }) => {
-  const currentRoute = url.pathname
+  const currentRoute = url.pathname;
 
   return {
-    currentRoute
-  }
-}
+    currentRoute,
+  };
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import sveltePreprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
+import adapter from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-  extensions: ['.svelte', '.md'],
+  kit: {
+    adapter: adapter(),
+  },
+  extensions: [".svelte", ".md"],
   preprocess: [
     sveltePreprocess(),
     mdsvex({
-      extensions: ['.md'],
-    })
+      extensions: [".md"],
+    }),
   ],
 };
 
