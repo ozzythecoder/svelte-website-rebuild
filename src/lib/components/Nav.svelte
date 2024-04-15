c47778504b494c50401177d19bc8a035a34039c2
August McAllister <august.chm@gmail.com>
2024-04-14 20:13:07 -0500

    Added toggle-able nav menu on mobile

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -7,14 +7,22 @@
     { name: "blog", url: "/blog" },
     { name: "music", url: "https://solo.to/ozzythepainter"},
     { name: "contact", url: "/contact" },
+    { name: 'gallery', url: '/gallery' },
     { name: "home", url: "/" },
   ]
+
+  const toggleMenu = () => {
+    const menu = document.getElementById('nav-menu')
+    menu.classList.toggle('visible')
+  }
 
 </script>
 
 <nav>
-  <ul>
-
+  <button id="menu-toggle" on:click={toggleMenu}>
+    <span class="material-symbols-outlined">menu</span>
+  </button>
+  <ul id="nav-menu" class="visible">
     {#each navRoutes as route}
       <li>
         <a href={route.url}>
@@ -22,39 +30,65 @@
         </a>
       </li>
     {/each}
-
   </ul>
 </nav>
 
 <style lang="scss">
-  nav ul {
+  @use '../styles/font-imports';
+  @use '../styles/fs-mixins';
+
+  #menu-toggle {
+    border: none;
+    background: none;
+    max-width: fit-content;
+    margin: 0 auto;
+    aspect-ratio: 1 / 1;
+    font: inherit;
+
+    display: none;
+  }
+
+  nav {
+    display: flex;
+    margin: 0 auto 0;
+    max-width: 1000px;
+    flex-direction: column;
+      @media screen and (max-width: 480px) {
+        margin-top: 1rem;
+        #menu-toggle {
+          display: block;
+        }
+      }
+  }
+
+  @media screen and (max-width: 480px) {
+    ul:not(.visible) {
+      height: 0px;
+    }
+  }
+
+  nav ul { 
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
-    
-    margin: 2rem auto 0;
+    align-items: center;
     padding: 0;
-    max-width: 1000px;
+    overflow: hidden;
+    transition: height 0.5s ease;
 
     @media screen and (max-width: 480px) {
       flex-direction: column;
-      align-items: center;
-      padding: 0;
     };
   }
 
   nav li {
+    padding: 0;
     list-style: none;
     text-transform: uppercase;
     font-family: Overpass, system-ui, sans-serif;
     font-weight: 200;
     font-size: 2rem;
-  }
-
-  @media screen and (min-width: 480px) {
-    nav li:not(:first-of-type) {
-      padding-left: 1rem;
-    }
+    @include fs-mixins.font-size(fs-mixins.$h2-font-sizes);
   }
 
   a {
