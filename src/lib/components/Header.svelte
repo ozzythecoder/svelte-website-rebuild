f8f4dbfedb86241e927d002ed0a953866d184896
August McAllister <august.chm@gmail.com>
2024-04-14 20:12:16 -0500

    Created generic Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,27 @@
+<header>
+  <div class="page-title">
+    <h1>
+      <a href="/">
+        Ozzy The Painter
+      </a>
+    </h1>
+  </div>
+</header>
+
+<style lang="scss">
+
+  @use '../styles/fs-mixins';
+
+  a {
+    font-family: Bitter, sans-serif;
+    text-transform: uppercase;
+    font-weight: 800;
+    @include fs-mixins.font-size(fs-mixins.$h2-font-sizes);
+    letter-spacing: 0.05rem;
+  }
+
+  .page-title {
+    text-align: center;
+    margin-top: 1.4rem;
+  }
+</style>
